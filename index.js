82b17004d0862199849eb6d85073af394656d24a
mazhang2718 <mazhang2718@gmail.com>
2016-10-25 16:40:53 -0500

    data looping

--- index.js
+++ index.js
@@ -18,21 +18,49 @@ app.get('/', function (req,res) {
    res.sendFile('index.html');
 });
 
+app.get('/one', function (req, res) {
+  res.sendFile(__dirname + '/public/index2.html');
+});
+
 
 io = io(server);
 
 io.on('connection', function(socket){
 
 
-request.get({
-  url: "https://api.nytimes.com/svc/news/v3/content/all/all.json",
-  qs: {
-    'api-key': process.env.NYT_API
-  },
-}, function(err, response, body) {
-  body = JSON.parse(body);
-  socket.emit('news', body);
-});
+	function sendRequest(){
+
+		request.get({
+		  url: "https://api.nytimes.com/svc/news/v3/content/all/all.json",
+		  qs: {
+		    'api-key': process.env.NYT_API
+		  },
+		}, function(err, response, body) {
+
+		  body = JSON.parse(body);
+		  var results = body.results;
+		  var abstracts = {};
+		  var abstract;
+		  var sent;
+
+		  for (var i=0; i<results.length; i++){
+		  	abstract = results[i].abstract;
+		  	sent = sentiment(abstract);
+		  	abstracts[i] = [abstract, sent];
+		  }
+		  
+		  socket.emit('news', abstracts);
+		});
+
+	}
+
+	sendRequest();
+
+	socket.on('newAbstractRequest', function(socket){
+		console.log('hi');
+		sendRequest();
+	});
+
 
 });
 
--- public/app.js
+++ public/app.js
@@ -1,64 +1,112 @@
 
 
-	var socket = io();
+//Initialize Client Socket Connection
+var socket = io();
 
-	var synth = new Tone.Synth().toMaster();
-
-	var scale = ["C4", "D4", "E4", "F4", "G4", "A4", "B4"];
-	var chordProg = ["Dm", "A7", "C7", "Fmaj", "D7", "Gmaj", "Cmaj"];
-
-	socket.on('news', function(data){
-
-		console.log(data);
-		var abstracts = [];
-		data = data.results;
-		for (var i = 0; i<data.length; i++){
-			abstracts.push(data[i].abstract);
-		}	
-
-		var ctr = abstracts.length - 1;
-		//var ctr = 7;
-		var noteIndex;
-
-		Tone.Transport.scheduleRepeat(function(time){
-			if (ctr>=0){
-				abstract = abstracts[ctr];
-
-				Math.seedrandom(abstract);
-				noteIndex = Math.floor(Math.random(abstract)*6);
-				chord = chordProg[noteIndex];
-
-	    		var pattern = new Tone.Pattern(function(time,note){
-					synth.triggerAttackRelease(note, "32n");
-				}, chordNotes[chord], "upDown");
-
-				pattern.start(time).stop("+4m");
-
-				var msg = new SpeechSynthesisUtterance(abstract);
-
-				var voices = window.speechSynthesis.getVoices();
-					msg.voice = voices[17]; // Note: some voices don't support altering params
-
-				window.speechSynthesis.speak(msg);
-
-	    		document.getElementById('test').innerHTML = abstract;
-
-	    		ctr -= 1;
-			}
-		}, "4:00:00");	
-
-	});
+//Initialize Instruments
+var synth = new Tone.Synth().toMaster();
+//var MelodyInst;
+//var BassInst;
+//var Percussion;
 
 
-	var chordNotes = {
-		"Dm" : ["D4", "F4", "C4", "A4"],
-		"A7" : ["G4", "C#4", "A4", "E4"],
-		"C7" : ["C4", "A#4", "E4", "F#4"],
-		"Fmaj" : [ "F4", "C4","D4", "A4"],
-		"D7" : ["C5", "F#4", "E5", "A#4"],
-		"Gmaj" : ["A4","D5", "F#4",  "B4"],
-		"Cmaj" : ["G4", "C5", "E5", "B4"],
-	};
+//Generate scale based on root note.
+function genScale(root){
 
 
-	Tone.Transport.start();
+}
+
+//Generate rhythms
+function genRhythms(){
+
+}
+
+//Generate all the treble notes/rhythms to be played
+function genTreb(){
+
+}
+
+//Generate chord progressions for each section
+var chordProgressions;
+
+var bpm = 120;
+var modes = "maj min".split(" ");
+var roots = "C Db D Eb E F F# G Ab A Bb".split(" ");
+
+
+
+
+var scale = ["C4", "D4", "E4", "F4", "G4", "A4", "B4"];
+var chordProg = ["Dm", "A7", "C7", "Fmaj", "D7", "Gmaj", "Cmaj"];
+
+//Retrieve new data from the news API
+
+socket.on('news', function(data){
+
+	var abstracts = [];
+	var sentiments = [];
+
+	for (var key in data) {
+	  if (data.hasOwnProperty(key)) {
+	    abstracts.push(data[key][0]);
+	    sentiments.push(data[key][1]);
+	  }
+	}
+
+	console.log(abstracts);
+	console.log(sentiments);
+
+	//var ctr = abstracts.length - 1;
+	var ctr = 2;
+	var noteIndex;
+
+	Tone.Transport.scheduleRepeat(function(time){
+		if (ctr>=0){
+			abstract = abstracts[ctr];
+
+			Math.seedrandom(abstract);
+			noteIndex = Math.floor(Math.random()*6);
+			chord = chordProg[noteIndex];
+
+    		var pattern = new Tone.Pattern(function(time,note){
+				synth.triggerAttackRelease(note, "32n");
+			}, chordNotes[chord], "upDown");
+
+			pattern.start(time).stop("+4m");
+
+			var msg = new SpeechSynthesisUtterance(abstract);
+
+			var voices = window.speechSynthesis.getVoices();
+				msg.voice = voices[17]; // Note: some voices don't support altering params
+
+			window.speechSynthesis.speak(msg);
+
+    		document.getElementById('test').innerHTML = abstract;
+
+    		ctr -= 1;
+
+    		if (ctr < 0){
+
+    			Tone.Transport.schedule(
+    				function(time){
+    					socket.emit('newAbstractRequest');
+    				}, "+4m");
+    		}
+    	}
+	}, "4m", "+0m", "12m");	
+
+});
+
+
+var chordNotes = {
+	"Dm" : ["D4", "F4", "C4", "A4"],
+	"A7" : ["G4", "C#4", "A4", "E4"],
+	"C7" : ["C4", "A#4", "E4", "F#4"],
+	"Fmaj" : [ "F4", "C4","D4", "A4"],
+	"D7" : ["C5", "F#4", "E5", "A#4"],
+	"Gmaj" : ["A4","D5", "F#4",  "B4"],
+	"Cmaj" : ["G4", "C5", "E5", "B4"],
+};
+
+
+Tone.Transport.start();
